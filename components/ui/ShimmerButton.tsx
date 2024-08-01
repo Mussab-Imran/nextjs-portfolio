67044221c276a6f4c9436d2747e9bc4ed2c30f27
Mussab-Imran <mussabimran7@gmail.com>
2024-08-01 17:19:49 -0400

    Completed Bento Grid section

--- components/ui/ShimmerButton.tsx
+++ components/ui/ShimmerButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ShimmerButton({ title, icon, position, handleClick, otherClasses } : {title: string; icon: React.ReactNode, position: string, handleClick?: () => void; otherClasses?: string;}) {
     return (
-        <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px]">
+        <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] " onClick={handleClick}>
             <span className="absolute inset-[-1000%] animate-[spin_5s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-small text-white backdrop-blur-3xl gap-2 ${otherClasses}">
                 {position === 'left' && icon}
