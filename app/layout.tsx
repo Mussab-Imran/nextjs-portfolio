1a589489cb0174cc54f32c66d0e3d683726dcaf7
Mussab-Imran <mussabimran7@gmail.com>
2024-08-03 19:54:33 -0400

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mussab Imran",
-  description: "An online portfolio that showcases Mussab's resume, work history and projects",
+  description: "Mussab Imran's online portfolio",
 };
 
 export default function RootLayout({
@@ -17,16 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import Homepage from "@/components/Homepage";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main  className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      {/* Justify-centre: center it vertically, items-centre: center it horizontally
-          flex-col: switch orientation, overflow-hidden: to hide the scrollbars */}
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome/>}
-        ]} />
-        <Hero />
-        {/* <Homepage/> */}
-        {/* <Grid /> */}
-      </div>
+    <main className="relative bg-black h-svh overflow-hidden">
+      <Hero />
     </main>
   );
 }
